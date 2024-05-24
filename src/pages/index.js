b1382253775af6edc32d9ea68702eb91a4774d4c
Sebastian Müller <sebastian@web3.foundation>
2024-05-24 15:28:06 +0200

    Add links to DF and JAM Prize to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,20 +15,47 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title} </h1> 
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="./docs/Process/how-to-apply">
-            Apply
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            to="./docs/rfps"
-            style={{
-              marginLeft: "20px"
-            }}>
-            Browse RFPs
-          </Link>
+        <div className={styles.buttons}            
+          style={{
+            display: "block"
+          }}
+          >
+          <div>
+            <Link
+              className="button button--secondary button--lg"
+              to="./docs/Process/how-to-apply">
+              Apply
+            </Link>
+            <Link
+              className="button button--secondary button--lg"
+              to="./docs/rfps"
+              style={{
+                marginLeft: "20px"
+              }}>
+              Browse RFPs
+            </Link>
+          </div>
+          <br />
+          <div>
+            <Link
+              className="button button--secondary button--lg"
+              to="https://futures.web3.foundation"
+              style={{
+                marginLeft: "20px",
+                backgroundColor: "rgb(250, 250, 250)"
+              }}>
+              Decentralized Futures <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#666" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+            </Link>
+            <Link
+              className="button button--secondary button--lg"
+              to="https://jam.web3.foundation"
+              style={{
+                marginLeft: "20px",
+                backgroundColor: "rgb(250, 250, 250)"
+              }}>
+              JAM Prize <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#666" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+            </Link>
+          </div>
         </div>
       </div>
     </header>
