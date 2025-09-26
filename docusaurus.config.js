7f8ffa6631da62df58da2fb0fb20a980291edca0
Sebastian Müller <sebastian@web3.foundation>
2025-09-26 10:35:26 +0200

    Update docusaurus.config.js
    Update announcement bar to newest blog post

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ module.exports = {
     announcementBar: {
       id: 'announcement',
       content:
-        '<a target="_blank" rel="noopener noreferrer" href="https://medium.com/web3foundation/web3-foundation-ecodev-wave-25-300a9e0933f7">Our blog post on Q1 2025 / Wave 25 is now online!</a>',
+        '<a target="_blank" rel="noopener noreferrer" href="https://medium.com/web3foundation/web3-foundation-ecodev-wave-26-7f145c39c2ef">Our blog post on Ecosystem Development Wave 26 is now online!</a>',
       backgroundColor: '#000',
       textColor: '#ffffff',
       isCloseable: true,
